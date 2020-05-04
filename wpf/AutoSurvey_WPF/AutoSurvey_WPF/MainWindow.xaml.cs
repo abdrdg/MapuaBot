011e07b6df95a261b965ac999cd9f2d8d3e9da64
Allan Bernard De Guzman <abeydeguzman@gmail.com>
2020-05-05 07:44:34 +0800

    Translated layout to wpf with rounded corners

---
+++ wpf/AutoSurvey_WPF/AutoSurvey_WPF/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoSurvey_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+
+        private void button_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_Hint_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Libre niyo ako milktea!! :)");
+        }
+    }
+}
